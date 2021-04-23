7a9379f6c6ece9acceb5d77f8f74150c6d7f91d4
sungwooksu <su@pocketgems.com>
2021-04-23 12:11:15 -0700

    Drag control

--- Assets/Scripts/TouchTrail.cs
+++ Assets/Scripts/TouchTrail.cs
@@ -2,49 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchTrail : MonoBehaviour
-{
-    public float power = 10f;
-    public float maxDrag = 5f;
-    public Rigidbody2D rb;
-    public LineRenderer lr;
 
-    Vector3 dragStartPos;
-    Touch touch;
-    private void Update() {
-        if (Input.touchCount > 0) {
-            touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began) {
-                DragStart();
-            }
-            if (touch.phase == TouchPhase.Moved) {
-                Dragging();
-            }
-            if (touch.phase == TouchPhase.Ended) {
-                DragRelease();
-            }
+public class TouchTrail : MonoBehaviour {
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    private float moveSpeed = 100f;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update () {
+        if (Input.GetMouseButton(0)) {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
         }
-    }
-    void DragStart() {
-        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
-        dragStartPos.z = 0f;
-        lr.positionCount = 1;
-        lr.SetPosition(0, dragStartPos);
-    }
-    void Dragging() {
-        Vector3 draggingPos =Camera.main.ScreenToWorldPoint(touch.position);
-        draggingPos.z = 0f;
-        lr.positionCount = 2;
-        lr.SetPosition(1, draggingPos);
-
-    }
-    void DragRelease() {
-        lr.positionCount = 0;
-        Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
-        dragReleasePos.z = 0f;
-
-        Vector3 force = dragStartPos - dragReleasePos;
-        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power ;
-        rb.AddForce(clampedForce, ForceMode2D.Impulse);
+        else {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
